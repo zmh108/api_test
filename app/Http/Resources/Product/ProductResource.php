71a0ef7d9d366c449651f34a3d5c3c439306f9de
zmh108 <zooz.alhawaj@gmail.com>
2023-10-19 15:53:15 +0300

    Modifying product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources\Product;
 
+use App\Models\Model\Product;
+use App\Models\Model\Review;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,8 +20,14 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'total_price' => round((1-($this->discount/100)) * $this->price, 2),
+            'rating' => $this->review->count() > 0 ? round($this->review->sum('star')/$this->review->count(),2) : 'No rating yet',
+            //'rating' => $this->reviews->sum('star')/$this->reviews->count(),
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
